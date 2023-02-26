eb9338e8dd08ccbcccaa38cc7dfa0fd2984223d5
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-02-26 15:46:32 +0100

    add

--- src/components/items.jsx
+++ src/components/items.jsx
@@ -5,6 +5,7 @@ const items = [
   { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
   { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
   { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+  { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
 ];
 
 export default items;
